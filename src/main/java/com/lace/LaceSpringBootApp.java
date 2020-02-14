37cc4de053c06cdc543a4ed9dcffec976c358a38
peter diei <okechukwu.diei2@gmail.com>
2020-02-14 23:59:01 +0100

    initial commit

---
+++ src/main/java/com/lace/LaceSpringBootApp.java
@@ -0,0 +1,12 @@
+package com.lace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaceSpringBootApp {
+
+	public static void main(String... args) {
+		SpringApplication.run(LaceSpringBootApp.class, args);
+	}
+}
